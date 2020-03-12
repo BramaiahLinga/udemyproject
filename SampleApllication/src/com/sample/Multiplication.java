3b809c257f6bcf5a0faebef4c56083c61b40755b
Bramaiah Linga <linga.bramaiah@gmail.com>
2020-03-12 21:26:38 +0530

    added multiplication

---
+++ SampleApllication/src/com/sample/Multiplication.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=100;
+		System.out.print(a*b);
+	}
+
+}
